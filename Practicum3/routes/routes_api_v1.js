60724c8d63ef2bd623fbf052a9af38c3ed55ce18
Mark <mamjong@avans.nl>
2017-05-11 16:05:54 +0200

    GET requests.

--- Practicum3/routes/routes_api_v1.js
+++ Practicum3/routes/routes_api_v1.js
@@ -3,6 +3,33 @@ var router = express.Router();
 var path = require('path');
 var pool = require('../db/dbconnector');
 
+router.get('/countries', function (req, res, next) {
+    var query = 'SELECT * FROM country';
+
+    pool.getConnection(function (err, connection) {
+        if (err) {throw error}
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if (err) {throw error}
+            res.status(200).json(rows);
+        });
+    });
+});
+
+router.get('/countries/:countryId', function (req, res) {
+    var countryId = req.params.countryId;
+    var query = 'SELECT * FROM country';
+
+    pool.getConnection(function (err, connection) {
+        if (err) {throw error}
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if (err) {throw error}
+            res.status(200).json(rows[countryId]);
+        })
+    })
+})
+
 router.get("*", function (req, res) {
     res.status(200);
     res.json({
