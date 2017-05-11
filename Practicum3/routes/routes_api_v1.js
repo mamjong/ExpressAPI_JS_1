85361a91ee0504153f0d7363cd2840499d80b8cf
mika krooswijk <mikakrooswijk@live.nl>
2017-05-11 15:58:13 +0200

    citie shit
    citei shit

--- Practicum3/routes/routes_api_v1.js
+++ Practicum3/routes/routes_api_v1.js
@@ -3,11 +3,31 @@ var router = express.Router();
 var path = require('path');
 var pool = require('../db/dbconnector');
 
-router.get("*", function (req, res) {
-    res.status(200);
-    res.json({
-        "Message" : "Welcome to API version 1, specify URL or search."
-    })
+router.get("/cities", function (req, res, next) {
+    var query = 'SELECT * FROM city';
+
+    pool.getConnection(function (err, connection) {
+        if(err){throw error}
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if(err){throw error}
+            res.status(200).json(rows);
+        });
+    });
+});
+
+router.get("/cities/:id", function (req, res, next) {
+    var id = req.params.id;
+    var query = 'SELECT * FROM city WHERE ID = ' + id;
+
+    pool.getConnection(function (err, connection) {
+        if(err){throw error}
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if(err){throw error}
+            res.status(200).json(rows);
+        });
+    });
 });
 
 module.exports = pool;
