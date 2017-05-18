da8325f4f463d0e3b47b187fa563200bb5903a53
Mark <mamjong@avans.nl>
2017-05-18 13:24:14 +0200

    POST countries.

--- Practicum3/routes/routes_api_v1.js
+++ Practicum3/routes/routes_api_v1.js
@@ -30,6 +30,43 @@ router.get('/countries/:countryId', function (req, res) {
     })
 })
 
+router.post("/countries", function (req, res, next) {
+    var code = req.body.Code;
+    var name = req.body.Name;
+    var continent = req.body.Continent;
+    var region = req.body.Region;
+    var surfaceArea = req.body.SurfaceArea;
+    var indepYear = req.body.IndepYear;
+    var population = req.body.Population;
+    var lifeExpectancy = req.body.LifeExpectancy;
+    var gnp = req.body.GNP;
+    var gnpOld = req.body.GNPOld;
+    var localName = req.body.LocalName;
+    var governmentForm = req.body.GovernmentForm;
+    var headOfState = req.body.HeadOfState;
+    var capital = req.body.Capital;
+    var code2 = req.body.Code2;
+
+    var query = {
+        sql : 'INSERT INTO `country`(Code, Name, Continent, Region, SurfaceArea, IndepYear, Population, LifeExpectancy, ' +
+        'GNP, GNPOld, LocalName, GovernmentForm, HeadOfState, Capital, Code2) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+        values : [code, name, continent, region, surfaceArea, indepYear, population, lifeExpectancy, gnp, gnpOld, localName,
+        governmentForm, headOfState, capital, code2],
+        timeout : 2000
+    };
+    console.log('query', query.sql);
+    res.contentType("application/json");
+    pool.getConnection(function (err, connection) {
+        if (err) {throw err}
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if (err) {throw err}
+            res.status(200).json(rows);
+        })
+    })
+
+});
+
 router.get("*", function (req, res) {
     res.status(200);
     res.json({
